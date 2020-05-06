85b80e7fb9aa3dc7c5bb14428115123a376509d4
Davidcreador <davidcreador@gmail.com>
2020-05-06 15:00:54 -0600

    Index to export all components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Navigation } from "./Navigation";
+export { default as Card } from "./Card";
+export { default as CharacterCard } from "./CharacterCard";
+export { default as DeathCard } from "./DeathCard";
+export { default as FilterButton } from "./FilterButton";
